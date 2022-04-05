5298dbdedf1c69182194fe7ef745934647917291
Ethan Simmelink <56223747+EthanSimm@users.noreply.github.com>
2022-04-05 18:24:13 -0400

    New Additions
    Added a constructor that takes a grid as it's parameter
    Added a generation variable to keep track of what generation the CGL is on
    Began the next generation method
    Changed the for statements in the toString to enhanced for loops

--- CGL.java
+++ CGL.java
@@ -3,6 +3,7 @@ package com.company;
 public class CGL {
     // Variables //
     private int[][] grid;
+    private int gen = 1;
 
     // Constructors //
     // Default constructor
@@ -10,18 +11,30 @@ public class CGL {
         grid = new int[8][8];
     }
 
+    // Constructor w/ a preset generation
+    public CGL(int[][] g){
+        grid = g;
+    }
+
     // Getters and setters //
     public int[][] getGrid() {return grid;}
+    public int getGen() {return gen;}
     public void setGrid(int[][] g) {grid = g;}
+    public void setGen(int g) {gen = g;}
 
     // Methods //
-    public String toString(){   // Not most effective
-        String temp = "";
-        for (int i = 0; i < grid[0].length; i++)
-        {
-            for (int j = 0; j < grid.length; j++)
-            {
-                temp += grid[i][j] + "  ";
+    // Simulating the next generation
+    public void nextGen(){
+        int[][] temp = new int[grid[0].length][grid.length];
+
+        // ** Insert looping, out of bounds checking, and rules here ** //
+    }
+
+    public String toString(){
+        String temp = "Generation " + gen + "\n";
+        for (int cellRow : grid[0]) {
+            for (int cellCol : grid[1]) {
+                temp += grid[cellRow][cellCol] + "  ";
             }
             temp += "\n";
         }
